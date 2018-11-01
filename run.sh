55311798c4cc5c555ff91f21a5283e2275a1823f
Aditya Gupta <rickyaditya.2711@gmail.com>
2018-11-02 00:15:45 +0530

    First Commit

---
+++ run.sh
@@ -0,0 +1,3 @@
+# python recognize_faces_video_file.py --encodings encodings.pickle --input videos/lunch_scene.mp4 --output output/lunch_scene_output.avi --display 0
+# python recognize_faces_video_file.py --encodings encodings.pickle --input videos/Salman_Akshay.mp4 --output output/Salman_Akshay.avi --display 1
+python recognize_faces_video_file.py --encodings encodings.pickle --input videos/Jurrasic_Park.mp4 --output output/Jurrasic_Park_live.avi --display 1
